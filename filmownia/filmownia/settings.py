ecb28a4f73cbf714b8f5ef6e1b10feffe46697f3
Joanna Kołakowska <kolakowska.joanna79@gmail.com>
2022-05-30 20:53:12 +0200

    wip

--- filmownia/filmownia/settings.py
+++ filmownia/filmownia/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'FilmowniaaConfig'
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+02:00'
 
 USE_I18N = True
 
