79cea50bda516ca3c7eac15d0bbb0cd9af0223cd
Joanna Kołakowska <kolakowska.joanna79@gmail.com>
2022-05-31 11:35:02 +0200

    update

--- filmownia/filmownia/settings.py
+++ filmownia/filmownia/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'FilmowniaaConfig'
-    'django_filters',
+    'filmowniaa.apps.FilmowniaaConfig',
+    # 'django_filters',
 ]
 
 MIDDLEWARE = [
